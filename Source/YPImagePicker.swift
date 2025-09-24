dddc0d708b8a007287ec6c6b520ac228af6fa251
Serhii Nelipa <serhii.nelipa@rademade.com>
2025-09-24 13:44:21 +0300

    - Add tint and background colors to config

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -73,8 +73,8 @@ open class YPImagePicker: UINavigationController {
         viewControllers = [picker]
         setupLoadingView()
         navigationBar.isTranslucent = false
-        navigationBar.tintColor = .ypLabel
-        view.backgroundColor = .ypSystemBackground
+        navigationBar.tintColor = YPImagePickerConfiguration.shared.colors.navigationBarTintColor
+        view.backgroundColor = YPImagePickerConfiguration.shared.colors.pickerBackgroundColor
 
         picker.didSelectItems = { [weak self] items in
             // Use Fade transition instead of default push animation
